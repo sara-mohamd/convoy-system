2dc616fe674473892d47ed11cebd2c2b616ef9fa
sara-mohamd <saraamohamed0038@gmail.com>
2025-05-04 03:57:35 +0300

    refactor: remove unused database and authentication utility files

--- src/core/config/db.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { configDotenv } from "dotenv";
-configDotenv
-const prisma = new PrismaClient()
-export default prisma
--- src/core/utils/password.utils.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt'
-
-export const hashPassword = (password: string) => {
-    return bcrypt.hash(password, 10)
-}
-export const comparePasswords = (password: string, hash: string): Promise<boolean> => {
-  return bcrypt.compare(password, hash)
-}
